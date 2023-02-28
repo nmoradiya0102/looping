6e302482465f9d9ff1a3fd45a750b0d53565980f
nmoradiya0102 <115994850+nmoradiya0102@users.noreply.github.com>
2023-02-28 17:31:35 +0530

    Add files via upload

--- looping-9.c
+++ looping-9.c
@@ -1,37 +1,19 @@
-#include<stdio.h>
-
+#include <stdio.h>
+ 
 int main()
 {
-	int n,i,fact=1;
-	
-	printf("Enter last number=");
-	scanf("%d",&n);
-	 i=1;
-	while(i<=n)
-	{
-		fact=fact*i;
-		i++;
-	}
-	printf("fact=%d",fact);
-	return 0;
+  int nbr, i = 1, f = 1;
+ 
+  printf("Enter a number to calculate its factorial: ");
+  scanf("%d", &nbr);
+ 
+  while(i <= nbr)
+  {
+      f = f * i;
+      i++;
+  }
+ 
+  printf("%d! = %ld\n", nbr, f);
+ 
+  return 0;
 }
-
-//#include <stdio.h>
-// 
-//int main()
-//{
-//  int nbr, i = 1, f = 1;
-// 
-//  printf("Enter a number to calculate its factorial: ");
-//  scanf("%d", &nbr);
-// 
-//  while(i <= nbr)
-//  {
-//      f = f * i;
-//      i++;
-//  }
-// 
-//  printf("%d! = %ld\n", nbr, f);
-// 
-//  return 0;
-//}
