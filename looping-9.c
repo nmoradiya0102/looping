6396057bbcf89ca8c53cd3a62a7ccdb8011bb61f
nmoradiya0102 <115994850+nmoradiya0102@users.noreply.github.com>
2023-02-28 17:28:51 +0530

    Add files via upload

--- looping-9.c
+++ looping-9.c
@@ -15,3 +15,23 @@ int main()
 	printf("fact=%d",fact);
 	return 0;
 }
+
+//#include <stdio.h>
+// 
+//int main()
+//{
+//  int nbr, i = 1, f = 1;
+// 
+//  printf("Enter a number to calculate its factorial: ");
+//  scanf("%d", &nbr);
+// 
+//  while(i <= nbr)
+//  {
+//      f = f * i;
+//      i++;
+//  }
+// 
+//  printf("%d! = %ld\n", nbr, f);
+// 
+//  return 0;
+//}
